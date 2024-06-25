9e242519d58bbb363b33dbfc611343794b773da0
yaxfer1 <66902009+yaxfer1@users.noreply.github.com>
2024-06-25 11:46:09 +0200

    Add files via upload

---
+++ SopraSteria_test.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soprasteria_test;
+
+/**
+ *
+ * @author yaxfe
+ */
+public class SopraSteria_test {
+    
+    
+    public static void par_impar(int num){
+    int lim = num%2;
+        while(num != lim){
+            System.out.println(num);
+            num -= 2;
+        }
+    System.out.println(num);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void stats_personas(int[][] personas){
+        int menores = 0;
+        int mayores = 0;
+        int ho18 = 0;
+        int mu18 = 0;
+        for(int i = 0; i < personas.length; i++){
+            if(personas[i][0]>= 18){
+            mayores++;
+                if(personas[i][1] == 0){ho18++;}
+            }
+            else{
+            menores++;
+                if(personas[i][1] == 1){mu18++;}
+            
+            }
+        
+        }
+        float porcentajeMayores= mayores * 100/50;
+        float porcentajeMujeres = (mayores - ho18 + mu18) *100/50;
+        System.out.println("Mayores de edad: " + mayores);
+        System.out.println("Menores de edad: " + menores);
+        System.out.println("Hombres mayores de edad: " + ho18);
+        System.out.println("Mujeres menores de edad: " + mu18);
+        
+        System.out.println("Porcentaje de mayores de edad: " + porcentajeMayores);
+        System.out.println("Porcentaje de mujeres: " + porcentajeMujeres);
+        
+    
+    }
+    
+    
+    public static void salario(int horas_trabajadas, float tarifa){
+        float salsem = 0;
+        
+        if(horas_trabajadas <= tarifa){
+            salsem = horas_trabajadas * tarifa;
+        }
+        else{ salsem = (float)((40 * tarifa) + (horas_trabajadas - 40) * (tarifa*1.5));}
+        
+        
+        System.out.println("Salario semanal del trabajador: " + salsem);
+    }
+    
+    public static int LEER_NUMERO(int numero){
+        return numero;
+    }
+    
+    public static int[][] LEER_PERSONAS(int[][] persona){
+        return persona;
+    }
+    
+    public static int LEER_HORASTRABAJADAS(int horas){
+        return horas;
+    }
+    
+    public static float LEER_TARIFA(float tarifa){
+        return tarifa;
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        int numero = LEER_NUMERO(2);
+        par_impar(numero);
+        
+        
+        int[][] personas = new int[][] {{}};
+                
+        
+        //Primer elemento edad, segundo sexo.
+        //El sexo se representa con 0 en hombre; 1 en mujer.
+        int[][] personasex = new int[][]{{25, 0}, {34, 1}, {18, 0}, {22, 1}, {45, 0}, {30, 1}, {17, 0}, {19, 1}, {20, 0}, {33, 1},
+                {40, 0}, {21, 1}, {60, 0}, {29, 1}, {15, 0}, {50, 1}, {16, 0}, {28, 1}, {23, 0}, {27, 1},
+                {19, 0}, {32, 1}, {44, 0}, {25, 1}, {38, 0}, {31, 1}, {20, 0}, {18, 1}, {21, 0}, {35, 1},
+                {36, 0}, {24, 1}, {43, 0}, {26, 1}, {22, 0}, {37, 1}, {48, 0}, {34, 1}, {25, 0}, {29, 1},
+                {17, 0}, {33, 1}, {39, 0}, {31, 1}, {18, 0}, {27, 1}, {45, 0}, {23, 1}, {50, 0}, {19, 1}
+                };
+        
+        personas = LEER_PERSONAS(personasex);
+        stats_personas(personas);
+        
+        int HORASTRABAJADAS = LEER_HORASTRABAJADAS(50);
+        float TARIFA = LEER_TARIFA(10);
+        salario(HORASTRABAJADAS, TARIFA);
+    }
+    
+}
